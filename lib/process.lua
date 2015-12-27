f82c24a3ae928bad3d14773a918001514e54146f
Konstantin Neshin <b.m.kast@gmail.com>
2015-12-27 08:57:49 +0300

    lib.process: do not query LFS and /proc until the first use

--- lib/process.lua
+++ lib/process.lua
@@ -10,24 +10,6 @@ module("obvious.lib.process")
 -- some code was taken from here:
 -- http://awesome.naquadah.org/wiki/Autostart#The_native_lua_way
 
-local check_state, err = pcall(function()
-  -- stupid test for /proc availability:
-  -- check if init's cmdline exists
-  local file_to_check = '/proc/1/cmdline'
-  if not os.rename(file_to_check, file_to_check) then
-    error("/proc filesystem seems to be absent")
-  end
-
-  -- now try loading LuaFileSystem
-  local lfs = require('lfs')
-
-  -- if the above line didn't raise an error,
-  -- then we have both /proc and LFS, so just
-  -- return the LFS instance
-
-  return lfs
-end)
-
 local function escape_regexp(string)
   return string:gsub("[-+?*]", {
     ["+"]  = "%+", ["-"] = "%-",
@@ -35,42 +17,62 @@ local function escape_regexp(string)
   })
 end
 
-if check_state then
-  local lfs = err
+local function _init_module()
+  local check_state, err = pcall(function()
+    -- stupid test for /proc availability:
+    -- check if init's cmdline exists
+    local file_to_check = '/proc/1/cmdline'
+    if not os.rename(file_to_check, file_to_check) then
+      error("/proc filesystem seems to be absent")
+    end
 
-  function iterate_processes(process)
-    local process_escaped = escape_regexp(process)
+    -- now try loading LuaFileSystem
+    local lfs = require('lfs')
 
-    local function yield_process()
-      for dir in lfs.dir('/proc') do
-        if tonumber(pid) ~= nil then
-          local f, err = io.open("/proc/" .. pid .. "/cmdline")
-          if f then
-            local cmdline = f:read("*all")
-            f:close()
-            if cmdline ~= "" then
-              coroutine.yield(cmdline)
+    -- if the above line didn't raise an error,
+    -- then we have both /proc and LFS, so just
+    -- return the LFS instance
+
+    return lfs
+  end)
+
+  if check_state then
+    local lfs = err
+
+    function iterate_processes(process)
+      local process_escaped = escape_regexp(process)
+
+      local function yield_process()
+        for dir in lfs.dir('/proc') do
+          if tonumber(pid) ~= nil then
+            local f, err = io.open("/proc/" .. pid .. "/cmdline")
+            if f then
+              local cmdline = f:read("*all")
+              f:close()
+              if cmdline ~= "" then
+                coroutine.yield(cmdline)
+              end
             end
           end
         end
       end
+
+      return coroutine.wrap(yield_process)
     end
 
-    return coroutine.wrap(yield_process)
-  end
+  else
 
-else
+    function iterate_processes()
+      fd = io.popen("ps -ewwo args")
+      if fd then
+        return fd:lines()
+      end
 
-  function iterate_processes()
-    fd = io.popen("ps -ewwo args")
-    if fd then
-      return fd:lines()
+      -- return an empty function in case of failure
+      return function () end
     end
 
-    -- return an empty function in case of failure
-    return function () end
   end
-
 end
 
 local processCache = {}
@@ -78,6 +80,10 @@ local processCache = {}
 function processCache:init()
   local cache = {}
   self._cache = cache
+
+  if not iterate_processes then
+    _init_module()
+  end
   
   for cmdline in iterate_processes() do
     table.insert(cache, cmdline)
@@ -87,6 +93,10 @@ function processCache:init()
 end
 
 function find_process(process)
+  if not iterate_processes then
+    _init_module()
+  end
+  
   for cmdline in iterate_processes() do
     if cmdline:find(process) then
       return cmdline
